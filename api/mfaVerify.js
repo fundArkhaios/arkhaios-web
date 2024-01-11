1db71b7abaecae49e23387adc2461faaf557907d
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-01-11 12:41:18 -0500

    Add files via upload

---
+++ api/mfaVerify.js
@@ -0,0 +1,23 @@
+const speakeasy = require('speakeasy');
+const aes = require('./aes.js');
+
+module.exports = {
+    method: "GET",
+    router: "/api/mfaVerify",
+    authenticate: true,
+    api: async function(req, res, usr) {
+        var { usrToken } = req.body;
+        var base32Encrypted = usr.base32Secret;
+
+        var base32 = aes.backward(base32Encrypted);
+
+        var verified = speakeasy.totp.verify({secret: base32,
+                                              encoding: 'base32',
+                                              token: usrToken});
+
+        if(!verified)
+            res.status(401).json({token: verified, err: "Incorrect Token"});
+
+        res.status(200).json({token: verified});
+    }
+}
