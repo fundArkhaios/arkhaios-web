e3a47294e2deee560b8477d1ca7892e0f7298773
Christian Z <christianzski@gmail.com>
2024-01-25 01:21:37 -0500

    Merge login/MFA verify, changing API responses

--- api/account/login.js
+++ api/account/login.js
@@ -1,6 +1,9 @@
 const db = require('../../util/db');
 const { v4: uuidv4 } = require('uuid');
 const { createHash } = require('crypto');
+const speakeasy = require('speakeasy');
+const aes = require('../aes.js');
+const RESPONSE_TYPE = require('../response_type.js');
 
 module.exports = {
     route: "/api/login",
@@ -8,50 +11,78 @@ module.exports = {
     post: async function (req, res) {
         try {
             var error = '';
+            let data = {};
+
+            let response = RESPONSE_TYPE.FAILED;
 
             const { email, password } = req.body;
 
             var hash = createHash('sha256').update(password).digest('hex');
-
-            const session = uuidv4();
-            const sessionExpiry = 1000 * 3600 * 5; // By default, expire in 5 hours
-
-            let mfa = false;
+            
+            const sessionExpiry = 1000 * 3600 * 5; // By default, sessions expire in 5 hours
 
             await db.connect(async (db) => {
                 try {
-                    const result = await db.collection('Users').
+                    const user = await db.collection('Users').
                     findOne({ email: email, password: hash });
 
-                    if (result) {
-                        mfa = result.mfaVerified;
-                        await db.collection('Users').updateOne(
-                            {
-                                "email": email,
-                                "password": hash
-                            },
-                            {
-                                $set: {
-                                    sessionToken: session,
-                                    sessionExpiry: Date.now() + sessionExpiry
-                                }
+                    if (user) {
+                        let authenticate = false;
+
+                        if(user.mfaVerified) {
+                            if(!req.body.mfa) data["mfa"] = true;
+                            else {
+                                var base32Encrypted = user.base32Secret;
+
+                                var base32 = await aes.backward(base32Encrypted);
+                        
+                                var verified = speakeasy.totp.verify({secret: base32,
+                                                                    encoding: 'base32',
+                                                                    token: req.body.mfa});
+                        
+                                if(verified) {
+                                    authenticate = true;
+                                } else error = 'invalid mfa code';
                             }
-                        );
+                        } else authenticate = true;
+
+                        if(authenticate) {
+                            response = RESPONSE_TYPE.SUCCESS;
+
+                            const session = uuidv4();
+
+                            data["session"] = session;
+
+                            await db.collection('Users').updateOne(
+                                {
+                                    "email": email,
+                                    "password": hash
+                                },
+                                {
+                                    $set: {
+                                        sessionToken: session,
+                                        sessionExpiry: Date.now() + sessionExpiry
+                                    }
+                                }
+                            );
+                        }
                     } else error = 'invalid credentials';
                 } catch(e) {
+                    console.log(e);
                     error = 'server error';
+                    response = RESPONSE_TYPE.ERROR;
                 }
             });
 
             if(error != '') {
-                res.status(401).json({error: error});
+                res.status(401).json({status: response, error: error});
             } else {
-                res.cookie('email', email, { maxAge: sessionExpiry, httpOnly: true });
-                res.cookie('session', session, { maxAge: sessionExpiry, httpOnly: true });
+                res.cookie('email', email, { maxAge: sessionExpiry, httpOnly: true, sameSite: true});
+                res.cookie('session', data.session, { maxAge: sessionExpiry, httpOnly: true, sameSite: true });
 
-                var ret = { token: session, error: error, mfa: mfa };
+                var ret = { status: response, message: error, data: data };
                 res.status(200).json(ret);
             }
-        } catch (e) { res.status(401).json({error: 'server error'}); }
+        } catch (e) { console.log(e); res.status(401).json({status: RESPONSE_TYPE.ERROR, message: 'server error'}); }
     }
 }
