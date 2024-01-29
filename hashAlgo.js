96c7d0a8b69c03ec43cf16621013d48dd9f8505b
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-01-28 20:18:25 -0500

    Add files via upload

---
+++ hashAlgo.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const pepp = process.env.PEPPER;
+
+//pre-condition: parameters are as follows{
+//               plain: plaintext string that is needed to be hashed
+//               salt: the user salt from the database, if no salt in DB, just input ''
+//               iterations: the user iterations in the database, if no iter in DB, just input 0
+//}
+//post-condition: the following are returned:{
+//                hashed string
+//                salt
+//                iterations
+//}
+module.export = {
+    hash: async function(plain, salt, iterations){
+        //this is checking to see if the user already has a generated salt
+        var tempSalt = salt;
+        if(salt == '') tempSalt = generateSalt();
+
+        //same thing, except for checking for amount of iterations for the SHA512
+        var tempIter = iterations;
+        if(iterations == 0) tempIter = generateIter();
+
+        //call the password-based key derivation function with pepper prepended to the plaintext, 64bit keyspace and SHA512 as the algo
+        var result = crypto.pbkdf2Sync(pepp + plain, tempSalt, tempIter, 64, 'sha512');
+        
+        //this returns 3 things, the hashed string, salt, and iterations
+        return {
+            hashed: result.toString('hex'), 
+            salt: tempSalt, 
+            iter: tempIter
+        };
+    },
+
+    //a function to just generate a random salt
+    generateSalt: function(){
+        return crypto.randomBytes(128).toString('base64');
+    },
+
+    //a function to generate a random iteration
+    generateIter: function(){
+        //this SHOULD generate a random int from 1,000 - 10,000
+        return Math.floor(Math.random() * 10000) + 1000;
+    }
+}
