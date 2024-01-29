67a01301154ad0c2e1d5e0f2e2685166b803a7ff
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-01-29 14:05:39 -0500

    Add files via upload

---
+++ api/PlaidIntegration/plaidTokens.js
@@ -0,0 +1,41 @@
+const { client, configs } = require('../plaidConfigs');
+const aes = require('../aes');
+const db = require('../util/db');
+
+module.exports = {
+    authenticate: true,
+    linkToken: async function(req, res){
+        var tokenResponse = await client.linkTokenCreate(configs);
+
+        res.status(201).json(tokenResponse.data);
+    },
+
+    exchangePublicToken: async function(publicAccessToken, usr){
+        var response = await client.itemPublicTokenExchange({
+            public_token: publicAccessToken,
+        });
+
+        var permAccessToken = response.data.access_token;
+        var encryptPlaid = aes.forward(permAccessToken);
+
+        //store the permanent access token in the database, ENCRYPTED
+        try{
+            await db.connect(async (db) => {
+                let results = await db.collection('Users').findOne({usr});
+
+                if(results){
+                    await db.collection('Users').updateOne({usr}, 
+                        {$push: {"plaidAccess": encryptPlaid}});
+                }
+            });
+        }
+        catch(e){
+            var error = e.toString();
+        }
+
+        if(!error)
+            res.status(201).json({flag: "success"});
+
+        res.status(501).json({flag: "failure", err: error});
+    }
+}
