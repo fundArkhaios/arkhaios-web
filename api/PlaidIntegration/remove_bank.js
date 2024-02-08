91021de2dd8d9bdf150047473352d751f3bfc641
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-02-08 13:45:20 -0500

    Add files via upload

---
+++ api/PlaidIntegration/remove_bank.js
@@ -0,0 +1,38 @@
+const { client } = require('../plaid_configs');
+const db = require('../util/db');
+const { backward } = require('../aes');
+
+module.export = {
+    route: '/PlaidIntegration/remove_bank',
+    method: POST,
+    authenticate: true,
+    post: async function(req, res, user){
+        var accessToken = backward(user.plaidAccess);
+        var error = "";
+
+        const request = {
+            access_token: accessToken
+        }
+
+        try{
+            var response = await client.itemRemove(request);
+
+            if(!response)
+                res.status(500).json({error: "Something went wrong"});
+
+            await db.connect(async (db) => {
+                let results = await db.collection('Users').findOne({user});
+
+                if(results){
+                    await db.collection('Users').deleteOne({user},
+                        {plaidAccess: accessToken});
+                }
+            });
+        }
+        catch(err){
+            error = err.toString();
+        }
+
+        res.status(200).json({flag: "Success! Bank Account deleted."});
+    }
+}
