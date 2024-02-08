410ecf1d9ca43edae35dc422993764a8fbc93442
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-02-08 13:25:39 -0500

    Add files via upload

---
+++ api/PlaidIntegration/public_token_exchange.js
@@ -0,0 +1,36 @@
+const client = require('../plaid_configs');
+const db = require('../util/db');
+const { forward } = require('../aes');
+
+module.export = {
+    route: '/PlaidIntegration/public_token_exchange',
+    method: GET,
+    authenticate: true,
+    post: async function(req, res, publicAccessToken, user){
+        var response = client.itemPublicTokenExchange({
+            public_token: publicAccessToken
+        });
+
+        var error = "";
+        var accessToken = forward(response.data.access_token);
+
+        try{
+            await db.connect(async (db) => {
+                let results = await db.collection('Users').findOne({user});
+
+                if(results){
+                    await db.collection('Users').updateOne({user},
+                        {$push: {"plaidAccess": accessToken}});
+                }
+            });
+        }
+        catch(e){
+            error = e.toString();
+        }
+
+        if(!error)
+            res.status(201).json({flag: "success"});
+
+        res.status(501).json({flag: "failure", err: error});
+    }
+}
