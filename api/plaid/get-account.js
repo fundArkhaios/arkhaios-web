f42f878e26a4d29d293dadb51b836b75df7d9565
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-02-20 12:50:33 -0500

    Update get-account.js
    fixed (again)

--- api/plaid/get-account.js
+++ api/plaid/get-account.js
@@ -1,15 +1,16 @@
 const { backward } = require('../aes');
 const { client } = require('../plaid_configs');
 
-
-async function getAccount(user){
-    var userAccess = backward(user.plaidAccess);
+module.exports = {
+    getAccount: async function (user) {
+        var userAccess = backward(user.plaidAccess);
     
-    var request = {
-        access_token: userAccess,
+        var request = {
+            access_token: userAccess,
+        }
+
+        var account = client.authGet(request);
+
+        return account; 
     }
-
-    var account = client.authGet(request);
-
-    return account;
 }
