5f305426defa3ee55cdcfcebebb6a90e003ebda4
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-02-20 12:43:31 -0500

    Update get-account.js
    fixed it from being an export to a normal function

--- api/plaid/get-account.js
+++ api/plaid/get-account.js
@@ -1,24 +1,15 @@
-const { client } = require('../plaid_configs');
 const { backward } = require('../aes');
+const { client } = require('../plaid_configs');
 
-module.exports = {
-    route: '/api/plaid/get-account',
-    authenticate: true,
-    get: async function(req, res, user){
-        const accessToken = backward(user.plaidAccess);
 
-        const request = {
-            access_token: accessToken,
-        }
-
-        const response = client.authGet(request);
-
-        res.json(response);
+async function getAccount(user){
+    var userAccess = backward(user.plaidAccess);
+    
+    var request = {
+        access_token: userAccess,
     }
+
+    var account = client.authGet(request);
+
+    return account;
 }
-
-
-//to do
-//Finish the get accounts to retrieve the account ID
-//get account ID for processor token endpoint
-//store the item_id in the database for the public_token_exchange endpoint
