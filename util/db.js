f0657f5d3b3c28007196484314871459bce2e51d
Joseph <limbfao@proton.me>
2024-02-21 17:36:33 -0500

    Remove some console logs

--- util/db.js
+++ util/db.js
@@ -2,10 +2,9 @@ const { MongoClient } = require("mongodb");
 const { createClient } = require('redis')
 
 let redisClient;
-console.log(redisClient);
+
 function getRedisClient() {
   if (!redisClient) {
-    console.log("No Redis Client!")
     redisClient = createClient({
       url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_SERVER}:${process.env.REDIS_PORT}`
     });
