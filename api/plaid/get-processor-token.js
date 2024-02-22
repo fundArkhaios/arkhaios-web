fea3f2ffe833828d9e8341b3ed319c6a493f0a2b
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-02-22 16:19:46 -0500

    Update get-processor-token.js

--- api/plaid/get-processor-token.js
+++ api/plaid/get-processor-token.js
@@ -4,7 +4,7 @@ module.exports = {
     route: '/api/plaid/get-processor-token',
     authenticate: true,
     get: async function (req, res, user){
-        var processor_token = backward(user.processor_token);
+        var processor_token = await backward(user.processor_token);
 
         res.status(200).json({token: processor_token});
     }
