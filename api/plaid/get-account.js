750c45f427f883fa4cd2add44b507d06c8859e2a
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-03-04 14:18:35 -0500

    Update get-account.js
    commented up the get-account

--- api/plaid/get-account.js
+++ api/plaid/get-account.js
@@ -2,15 +2,21 @@ const { backward } = require('../aes');
 const { client } = require('../plaid_configs');
 
 module.exports = {
+    //it should be noted that this is not an API endpoint, this is merely an internal function to grab the account
+    //this function is to be used in other endpoints
     getAccount: async function (user) {
+        //decrypt plaid access token
         var userAccess = backward(user.plaidAccess);
-    
+
+        //create request object
         var request = {
             access_token: userAccess,
         }
 
+        //call the authGet endpoint from Plaid
         var account = client.authGet(request);
 
+        //return the account
         return account; 
     }
 }
