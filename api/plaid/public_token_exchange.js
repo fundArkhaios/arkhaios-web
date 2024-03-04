495cc74309db4d54687099f6b6a3cb43e8a2c419
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-03-04 14:22:55 -0500

    Update public_token_exchange.js
    commenting public-token-exchange

--- api/plaid/public_token_exchange.js
+++ api/plaid/public_token_exchange.js
@@ -6,20 +6,23 @@ module.exports = {
   route: "/api/plaid/public-token-exchange",
   authenticate: true,
   post: async function (req, res, user) {
+    //call the itemPublicTokenExchange endpoint from Plaid, this gives us the access token
+    //public_token is given to use from the front end after the link completion.
     var response = await client.itemPublicTokenExchange({
       public_token: req.body.public_token,
     });
     
     var error = "";
+    //encrypt both the access token and the "item_id"
+    //item_id is the reference id for the user's individual account with Plaid. 
     var accessToken = await forward(response.data.access_token);
     var item_id = await forward(response.data.item_id);
-    
+
+    //store the stuff in the database
     try {
       await db.connect(async (db) => {
-        console.log("HI");
         try {
           let results = await db.collection("Users").findOne({email: user.email});
-          console.log("hello");
           if (results) {
             // console.log(results);
             await db.collection("Users").updateOne({"email": user.email}, {
@@ -36,10 +39,8 @@ module.exports = {
 
     if (!error) {
       res.status(201).json({ flag: "success" });
-      console.log('good');
     } else {
       res.status(501).json({ flag: "failure", err: error });
-      console.log('bad');
     }
   },
 };
