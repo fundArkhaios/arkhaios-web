db19fcd4fb9f72b6395afdd7a80cc2a32bbb6789
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-03-04 14:20:02 -0500

    Update get-processor-token.js
    comment get-processor-token

--- api/plaid/get-processor-token.js
+++ api/plaid/get-processor-token.js
@@ -1,6 +1,8 @@
 const { backward } = require('../aes');
 
 module.exports = {
+    //should be noted that this is not meant to be an endpoint. This should be an internal function used by other endpoints
+    //I don't want the processor token to be passesd to the frontend in any way because that is not super secure
     get_processor_token: async function (user){
         var processor_token = backward(user.processor_token);
 
