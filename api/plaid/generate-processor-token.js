4fa14f50e1e3aeee8f15ab6b91cdd07f9f4e97a5
dwest21 <66216412+dwest21@users.noreply.github.com>
2024-03-04 14:16:39 -0500

    Update generate-processor-token.js
    add comments to processor token

--- api/plaid/generate-processor-token.js
+++ api/plaid/generate-processor-token.js
@@ -7,11 +7,20 @@ module.exports = {
     route: '/api/plaid/generate-processor-token',
     authenticate: true,
     get: async function (req, res, user){
+        //this takes the user's access token and decrypts it
         var accessToken = await backward(user.plaidAccess);
+        //call the getaccount endpoint from Plaid to have access to their items.
+        //"Items" in this case refers to the objects stored within the user object in PLaid's database
+        //The other objects in here refer to the user's Plaid ID, bank account's connected, and personal info
+        //The full json object returned can be seen at https://plaid.com/docs/api/accounts/#accountsget
         var userAccount = plaidAccount.getAccount(user);
 
+        //grab their account from Plaid
         var user_id = userAccount.account_id;
 
+        //create a request object. The information necessary is their Plaid ID, access token, and the payment
+        //processor that we are going to be using. This can be changed to dwolla, alpaca, or whatever processor
+        //plaid has a list of payment procecssor partners on their doc website
         var request = {
             access_token: accessToken,
             account_id: user_id,
@@ -19,9 +28,14 @@ module.exports = {
         }
 
         try{
+            //call the processor token endpoint from Plaid with the request object
             const response = client.processorTokenCreate(request);
+            //encrypt the processor token. The response is an object of objects. 
+            //response.data refers to the data object that is returned. There are a couple of fields that we could
+            //look at, but the only one that we should concern ourselves with is .data
             var encryptProcessorToken = forward(response.data.processor_token);
 
+            //if there was a response, connect to the database and store the processor token for later use
             if(response){
                 await db.connect(async (db) => {
                     let results = await db.collection('Users').findOne(user);
