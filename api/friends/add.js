cccddc4ec5cd870de7cf1c458fc0b6601067a970
lukefaulkner2002 <lukefaulkner2002@gmail.com>
2024-03-05 16:13:54 -0500

    added block/remove apis, changed add for block functionality

--- api/friends/add.js
+++ api/friends/add.js
@@ -1,6 +1,6 @@
 const db = require('../../util/db');
 const { RESPONSE_TYPE, SERVER_ERROR } = require('../response_type');
-const { logger } = require('../../util/logger')
+const { logger } = require('../../util/logger');
 
 module.exports = {
     route: "/api/friends/add",
@@ -8,46 +8,61 @@ module.exports = {
     post: async function(req, res, user) {
         let { id } = req.body;
 
-        if(id == user.id) {
+        if (id == user.accountID) {
             res.status(200).json({status: RESPONSE_TYPE.FAILED, message: "cannot add self", data: {}})
+            return;
         }
 
         try {
             await db.connect(async (db) => {
                 try {
-                    const target = await db.collection('Users').findOne({
+                    const self = await db.collection('Friends').findOne({
+                        accountID: user.accountID
+                    });
+
+                    const target = await db.collection('Friends').findOne({
                         accountID: id
                     });
 
-                    if(target) {
-                        await db.collection('Friends').updateOne({
-                            user: id,
-                        }, {$addToSet: { sentRequests: id }}, { upsert: true })
+                    const userExists = await db.collection('Users').findOne({
+                        accountID: id
+                    });
+
+                    if (userExists) {
+                        // Check if either user is in the other's blocked list
+                        if (self?.blocked?.includes(id) || target?.blocked?.includes(user.accountID)) {
+                            res.status(200).json({status: RESPONSE_TYPE.FAILED, message: "cannot add a blocked user", data: {}})
+                            return;
+                        }
 
                         await db.collection('Friends').updateOne({
-                            user: user.accountID,
-                        }, {$addToSet: { receivedRequests: id }}, { upsert: true })
+                            accountID: id,
+                        }, {$addToSet: { receivedRequests: user.accountID }}, { upsert: true });
+
+                        await db.collection('Friends').updateOne({
+                            accountID: user.accountID,
+                        }, {$addToSet: { sentRequests: id }}, { upsert: true });
 
                         res.status(200).json({status: RESPONSE_TYPE.SUCCESS, message: "request sent", data: {}})
                     } else {
                         res.status(200).json({status: RESPONSE_TYPE.FAILED, message: "user does not exist", data: {}})
                     }
-                } catch(e) {
+                } catch (e) {
                     logger.log({
                         level: 'error',
                         message: e
-                    })
+                    });
         
-                    SERVER_ERROR(res)
+                    SERVER_ERROR(res);
                 }
-            })
-        } catch(e) {
+            });
+        } catch (e) {
             logger.log({
                 level: 'error',
                 message: e
-            })
+            });
 
-            SERVER_ERROR(res)
+            SERVER_ERROR(res);
         }
     }
-}
+};
