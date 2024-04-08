5736390caca7e3c3c19a84c54d8b9a8686655b83
Joseph <limbfao@proton.me>
2024-04-08 13:37:02 -0400

    Fixed watchlist bug

--- app/markets/stocks/stockChart.js
+++ app/markets/stocks/stockChart.js
@@ -65,7 +65,7 @@ export default function StockChart({ symbol }) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          noPeriodSymbol,
+          symbol: noPeriodSymbol
         }),
       });
       const data = await response.json();
