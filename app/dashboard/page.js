cca4803b2a8f806dcd84111be9381aadc53a2c4b
Joseph <limbfao@proton.me>
2024-04-11 12:24:47 -0400

    Added welcome name for fund dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -36,6 +36,8 @@ export default async function Page() {
 
     return (
             <div className ="h-screen overflow-x-hidden px-10 py-5">
+                <div>Welcome, {user.firstName + " " + user.lastName } !</div>
+                <div className="text-lg">{fund.fundName + " | "} ${fund.fundSymbol}</div>
                 <div className = "grid grid-rows-3 cols-2 justify-items-center w-full gap-4">
                     <div className = "col-span-1 place-content-center"><TopLeft fund={fund}/></div>
                     <div className = "col-span-1"><TopRight fundSymbol={fund.fundSymbol} fundAvailableBalance={fund.availableBalance} fundID={fund.fundID}/></div>
