6bc1460217077f65fdd5f2cea8fc1668db20a7fe
Joseph <limbfao@proton.me>
2024-05-17 21:01:25 -0400

    Updated message picture and search bar color.

--- app/friends/messageChat.js
+++ app/friends/messageChat.js
@@ -180,10 +180,10 @@ export default function MessageChat({ friendSelected, websocket }) {
             ) : (
               <div className="chat chat-start px-2">
                 <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
+                  <div className="w-10 rounded-full"> 
                     <img
                       alt="Tailwind CSS chat bubble component"
-                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      src="/messagingPersonPicture.png"
                     />
                   </div>
                 </div>
